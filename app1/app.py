5dd69c9b6bd0e0fd85bb5c0565a177120a500903
Yousef Ebrahimi <Yousef.Ebrahimi@gmail.com>
2020-11-07 19:07:08 -0500

    - Working load balancer is implemented by adding two web apps (even though there are identical) and and nginx config to handle the load balancing.

---
+++ app1/app.py
@@ -0,0 +1,12 @@
+import flask
+
+app1 = flask.Flask(__name__)
+
+
+@app1.route('/')
+def hello_world():
+    return 'This is App 1 :) '
+
+
+if __name__ == '__main__':
+    app1.run(debug=True, host='0.0.0.0')
